bb6f0bd1d7194735a5b18f053f17fa7fd0674178
Sherwin George <89149622+SherwinGeorge@users.noreply.github.com>
2024-01-22 11:47:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ for(var i=0;i<document.querySelectorAll(".drum").length; i++){
     document.querySelectorAll(".drum")[i].addEventListener("click",function(event){
         var buttonInnerHTML = this.innerHTML;
         makeSound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML);
     });
 }
 
 document.addEventListener("keydown",function(event){
     makeSound(event.key);
+    buttonAnimation(event.key);
 });
 
 function makeSound(key){
@@ -44,3 +46,12 @@ function makeSound(key){
 
         }
     }
+
+function buttonAnimation(currentKey){
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    },100);
+}
